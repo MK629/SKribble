fb7474df9228e7fa4adb64247fc680f93cec9836
Artificial Idiot <myatkw629@gmail.com>
2025-05-02 00:24:33 +0630

    encapsulated secrets

--- Backend/SKribble/src/main/java/com/sKribble/api/controller/TestController.java
+++ Backend/SKribble/src/main/java/com/sKribble/api/controller/TestController.java
@@ -1,13 +1,17 @@
 package com.sKribble.api.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class TestController {
 	
+	@Value("${jwtToken}")
+	String token;
+	
 	@QueryMapping
 	public String greet() {
-		return "Hello World.";
+		return token;
 	}
 }
