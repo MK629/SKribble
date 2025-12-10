02fec2e98001c8bca4cca6c6a9259e96b6486ebf
mk <myatkw629@gmail.com>
2025-12-10 22:08:53 +0630

    removed redundant async function

--- Frontend/skribble/src/components/profile/MiniProfileCard.tsx
+++ Frontend/skribble/src/components/profile/MiniProfileCard.tsx
@@ -13,8 +13,8 @@ const MiniProfileCard = () => {
   const [profileInfo, setProfileInfo] = useState<UserInfo>();
   const router = useRouter()
 
-  const updateUserInfo = async () => {
-    await getCurrentUserInfo().then((response) => {setProfileInfo(response.getCurrentUserInfo)}).catch((e) => {toast.error(e.message)});
+  const updateUserInfo = () => {
+    getCurrentUserInfo().then((response) => {setProfileInfo(response.getCurrentUserInfo)}).catch((e) => {toast.error(e.message)});
   }
 
   const logoutAction = async () => {
@@ -28,13 +28,23 @@ const MiniProfileCard = () => {
 
   return (
     <div className="w-56 bg-gray-200 border-[1px] border-black shadow-xl rounded-xl p-4 flex flex-col gap-3">
-        <div className="flex flex-col gap-1 border-b-[1px] border-black pb-3">
-            <span className="font-semibold text-black dark:text-white">{profileInfo?.Username}</span>
-            <span className="text-sm text-gray-700 dark:text-gray-300">{profileInfo?.Email}</span>
-            <button className="flex items-center justify-center gap-1 mt-2 px-2 py-1 text-sm rounded-md border-[1px] border-black hover:bg-gray-100 dark:hover:bg-gray-800 transition">
-            <UserCog/>
-            View Detail
-            </button>
+        <div className="border-b-[1px] border-black pb-3">
+          {
+            profileInfo ? 
+            <div className="flex flex-col gap-1">
+              <span className="font-semibold text-black dark:text-white">{profileInfo?.Username}</span>
+              <span className="text-sm text-gray-700 dark:text-gray-300">{profileInfo?.Email}</span>
+              <button className="flex items-center justify-center gap-1 mt-2 px-2 py-1 text-sm rounded-md border-[1px] border-black hover:bg-gray-100 dark:hover:bg-gray-800 transition">
+              <UserCog/>
+              View Detail
+              </button>
+            </div>
+            :
+            <div>
+              Loading user info...
+            </div>
+          }
+
         </div>
 
         <button className="flex items-center justify-center gap-1 px-2 py-1 text-sm rounded-md border-[1px] border-black hover:bg-gray-100 dark:hover:bg-gray-800 transition">
